e32103d8a29760162d38f40e2bff6869082007c0
Salman <salmanejaz8125@gmail.com>
2023-01-01 16:18:58 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Lottery from "./Lottery";
 function App() {
   return (
     <div className="App">
-      <Lottery />
+      <Lottery tittle={"Lottery 1"} />
+      <Lottery tittle={"Lottery 2"} maxBalls={13} maxNums={10} />
     </div>
   );
 }
--- src/Lottery.js
+++ src/Lottery.js
@@ -5,30 +5,39 @@ class Lottery extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nums: [],
+      nums: Array.from({ length: this.props.maxBalls }),
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
     return (
       <section className="lottery">
         <h1>{this.props.tittle}</h1>
-        <div>balls go here</div>
+        <div>
+          {this.state.nums.map((n) => (
+            <Ball num={n} />
+          ))}
+        </div>
         <button onClick={this.handleClick}>Generate</button>
       </section>
     );
   }
 
-  static props = {
+  static defaultProps = {
     tittle: "Lottery Game",
-    maxNum: 40,
-    maxSize: 6,
+    maxBalls: 6,
+    maxNums: 40,
   };
 
   handleClick() {
-    const randomNumber = Math.floor(Math.random() * this.maxNum) + 1;
-    return randomNumber;
+    this.setState((currState) => ({
+      nums: currState.nums.map(
+        (n) => Math.floor(Math.random() * this.props.maxNums) + 1
+      ),
+    }));
   }
+  
 }
 
 export default Lottery;
